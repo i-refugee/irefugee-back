eb6ee32c14c7adf75d651e56fcdb1e8c6c60c1f7
Ubuntu <dimakos.achileas@gmail.com>
2016-03-04 19:42:36 +0200

    serializers_fix_1

--- app/serializers/keyword_serializer.rb
+++ app/serializers/keyword_serializer.rb
@@ -1,5 +1,6 @@
 class KeywordSerializer < ActiveModel::Serializer
   attributes :id, :keyword, :created_at, :updated_at
-
+  has_many :center_keywords
+  has_many :centers, through: :center_keywords
   belongs_to :need
 end
--- app/serializers/need_serializer.rb
+++ app/serializers/need_serializer.rb
@@ -1,3 +1,6 @@
 class NeedSerializer < ActiveModel::Serializer
   attributes :id, :name, :created_at, :updated_at
+  has_many :center_needs
+  has_many :centers, through: :center_needs
+  has_many :keywords
 end
